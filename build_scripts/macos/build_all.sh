853da8322a1cf2b318e4b0fa9941a3c31c242111
MarketKernel <global@marketkernel.com>
2023-11-27 08:35:41 +0200

    build experiment 1

--- build_scripts/macos/build_all.sh
+++ build_scripts/macos/build_all.sh
@@ -8,19 +8,18 @@ cd $SCRIPTS_DIR
 cd ../../external/monero-cpp/external/monero-project
 brew update && brew bundle --file=contrib/brew/Brewfile
 
-
 echo "***************"
 echo "*** unbound ***"
 echo "***************"
 
-
-cd ~
+#cd ~
+cd $SCRIPTS_DIR
 curl -O https://www.nlnetlabs.nl/downloads/unbound/unbound-1.17.0.tar.gz
 tar -xvzf unbound-1.17.0.tar.gz
 cd unbound-1.17.0
 ./configure --with-ssl=/usr/local/opt/openssl --with-libexpat=/usr/local/opt/expat
-#make
-#sudo make install
+make
+sudo make install
 
 
 echo "***************"
