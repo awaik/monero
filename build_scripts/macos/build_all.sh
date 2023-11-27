2d5d0adeda9422ae702b0675db43a39413075e32
MarketKernel <global@marketkernel.com>
2023-11-27 07:57:43 +0200

    fixes

--- build_scripts/macos/build_all.sh
+++ build_scripts/macos/build_all.sh
@@ -8,33 +8,25 @@ cd $SCRIPTS_DIR
 cd ../../external/monero-cpp/external/monero-project
 brew update && brew bundle --file=contrib/brew/Brewfile
 
-echo "***************"
-echo "*** libexpat ***"
-echo "***************"
-
-cd ~
-curl -O https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-2.4.8.tar.bz2
-tar -xf expat-2.4.8.tar.bz2
-cd expat-2.4.8
-./configure --enable-static --disable-shared
-make
-sudo make install
 
 echo "***************"
 echo "*** unbound ***"
 echo "***************"
 
+
 cd ~
 curl -O https://www.nlnetlabs.nl/downloads/unbound/unbound-1.17.0.tar.gz
 tar -xvzf unbound-1.17.0.tar.gz
 cd unbound-1.17.0
-./configure --with-libexpat=~/expat-2.4.8
+./configure --with-ssl=/usr/local/opt/openssl --with-libexpat=/usr/local/opt/expat
 make
 sudo make install
+
 
 echo "***************"
 echo "*** monero-cpp ***"
 echo "***************"
+
 
 cd $SCRIPTS_DIR
 cd ../../external/monero-cpp/external/monero-project
