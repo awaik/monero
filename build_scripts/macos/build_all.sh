5dd5bb7bf0dd62ca93f9551999cd0c5678840ee6
MarketKernel <global@marketkernel.com>
2023-11-27 06:28:27 +0200

    build script fixes

--- build_scripts/macos/build_all.sh
+++ build_scripts/macos/build_all.sh
@@ -4,6 +4,12 @@ export SCRIPTS_DIR=`pwd`
 
 HOST_NCORES=$(nproc 2>/dev/null || shell nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
 
+cd SCRIPTS_DIR
+cd ../../external/monero-cpp/external/monero-project
+brew update && brew bundle --file=contrib/brew/Brewfile
+
+cd SCRIPTS_DIR
+
 curl -O https://www.nlnetlabs.nl/downloads/unbound/unbound-1.17.0.tar.gz
 tar -xvzf unbound-1.17.0.tar.gz
 cd unbound-1.17.0
@@ -11,5 +17,4 @@ cd unbound-1.17.0
 
 cd SCRIPTS_DIR
 cd ../../external/monero-cpp/external/monero-project
-brew update && brew bundle --file=contrib/brew/Brewfile
 make release-static -j$HOST_NCORES
