0cdeed999bb18e85b86633a5091cbf23ec74a404
MarketKernel <global@marketkernel.com>
2023-11-28 10:16:03 +0200

    fixes

--- build_scripts/ios/build_boost.sh
+++ build_scripts/ios/build_boost.sh
@@ -2,16 +2,16 @@
 
 . ./config.sh
 
-MIN_IOS_VERSION=14.0
-BOOST_URL="https://github.com/cake-tech/Apple-Boost-BuildScript"
+MIN_IOS_VERSION=10.0
+BOOST_URL="https://github.com/cake-tech/Apple-Boost-BuildScript.git"
 BOOST_DIR_PATH="${SOURCE_DIR}/Apple-Boost-BuildScript"
 BOOST_VERSION=1.72.0
 BOOST_LIBS="random regex graph random chrono thread filesystem system date_time locale serialization program_options"
 
 echo "============================ Boost ============================"
 
-#echo "Cloning Apple-Boost-BuildScript from - $BOOST_URL"
-git clone $BOOST_URL $BOOST_DIR_PATH
+echo "Cloning Apple-Boost-BuildScript from - $BOOST_URL"
+git clone -b build $BOOST_URL $BOOST_DIR_PATH
 cd $BOOST_DIR_PATH
 ./boost.sh -ios \
 	--min-ios-version ${MIN_IOS_VERSION} \
