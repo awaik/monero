456d03f35c221f268d7dffa94e600e861ddd46de
MarketKernel <global@marketkernel.com>
2023-11-28 04:41:35 +0200

    fixes

--- build_scripts/ios/build_all.sh
+++ build_scripts/ios/build_all.sh
@@ -9,12 +9,12 @@ mkdir -p $BUILD_INCLUDE_DIR
 mkdir -p $DIST_LIB_DIR
 mkdir -p $DIST_INCLUDE_DIR
 
-#./install_missing_headers.sh
-#./build_openssl.sh
-#./build_boost.sh
-#./build_sodium.sh
-#./build_zmq.sh
-#./build_unbound.sh
+./install_missing_headers.sh
+./build_openssl.sh
+./build_boost.sh
+./build_sodium.sh
+./build_zmq.sh
+./build_unbound.sh
 ./build_monero.sh
 
 ./setup.sh
