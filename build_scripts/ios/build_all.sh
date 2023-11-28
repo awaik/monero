2005bfe4f94c496a76e1963bda048e4549659b82
MarketKernel <global@marketkernel.com>
2023-11-28 11:40:38 +0200

    build script fixes

--- build_scripts/ios/build_all.sh
+++ build_scripts/ios/build_all.sh
@@ -9,6 +9,13 @@ mkdir -p $BUILD_INCLUDE_DIR
 mkdir -p $DIST_LIB_DIR
 mkdir -p $DIST_INCLUDE_DIR
 
+cd $SCRIPTS_DIR
+cd ../../external/monero-cpp/external/monero-project
+brew update && brew bundle --file=contrib/brew/Brewfile
+brew remove hidapi
+
+cd $SCRIPTS_DIR
+
 ./install_missing_headers.sh
 ./build_openssl.sh
 ./build_boost.sh
